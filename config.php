8ca678f31554fe1965d2fd9ccc80d6bbe7bb5be2
NizalPerera <116760388+nisal485@users.noreply.github.com>
2026-06-13 20:14:56 +0530

    Create config.php

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+// Host settings for free hosting / local environment
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'online_store');
+
+$conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+if($conn === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+?>
